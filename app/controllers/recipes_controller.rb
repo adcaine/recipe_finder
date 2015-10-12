a813735225d0129b9c8e5e269bd6e871ff777fa7
Allan Caine <adcaine@gmail.com>
2015-10-11 20:53:20 -0400

    Final Corrections; All tests pass

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,6 @@
 class RecipesController < ApplicationController
   def index
     @search_term = params[:search] || 'chocolate'
-    puts "Search term is #{@search_term}"
     @recipes = Recipe.for(@search_term)
   end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,9 @@
 class Recipe
   include HTTParty
 
-  base_uri "http://www.food2fork.com/api" 
+  key_value = ENV['FOOD2FORK_KEY']
+  hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'www.food2fork.com'
+  base_uri "http://#{hostport}/api" 
   default_params key: '38a6bccce5ca111197b7b2fd50142388'
   format :json
 
